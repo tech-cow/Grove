17e9475482bc46a64656b8587dcae09406b64a5d
junior147147 <junior147147@github.com>
2014-11-12 17:00:28 -0700

    sign up page layout update

---
+++ php/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+include 'core/init.php';
+
+include 'includes/overall/header.php';
+	
+include'includes/header.php';
+
+include'includes/page.php';
+
+include'includes/footer.php';
+
+?>
+
+<?php include 'includes/overall/footer.php'; ?>
