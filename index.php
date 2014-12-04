ddec981d64947521de57d3cc53df80caeaf03381
root <root@localhost.localdomain>
2014-12-04 10:21:16 -0700

    change signin file, need main page directory

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!-- <!--
-?php 
+<?php 
 session_start();
-error_reporting(0);
+//error_reporting(0);
 mysql_connect('localhost'.'root','');
 mysql_select_db(login); 
 function sanitize($data){
@@ -16,7 +16,45 @@ function user_active($username){
 }
 $errors = array();
 ?>
+
 !-->
+
+<?php
+session_start(); // Starting Session
+$error=''; // Variable To Store Error Message
+if (isset($_POST['submit'])) {
+if (empty($_POST['username']) || empty($_POST['password'])) {
+$error = "Username or Password is invalid";
+}
+else
+{
+// Define $username and $password
+$username=$_POST['username'];
+$password=$_POST['password'];
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+$connection = mysql_connect("localhost", "root", "");
+// To protect MySQL injection for Security purpose
+$username = stripslashes($username);
+$password = stripslashes($password);
+$username = mysql_real_escape_string($username);
+$password = mysql_real_escape_string($password);
+// Selecting Database
+$db = mysql_select_db("USERS", $connection);
+// SQL query to fetch information of registerd users and finds user match.
+$query = mysql_query("select * from USERS  where password='$password' AND username='$username'", $connection);
+$rows = mysql_num_rows($query);
+if ($rows == 1) {
+$_SESSION['login_user']=$username; // Initializing Session
+header("location: mainpage.php"); //???????????????????????????? Redirecting To Other Page
+} else {
+$error = "Username or Password is invalid";
+}
+mysql_close($connection); // Closing Connection
+}
+}
+?>
+
+
 
 <DOCTYPE <html>
 		
@@ -48,10 +86,10 @@ $errors = array();
 					<input type ="password" name="password">
 				</p>
 				<p>
-					<input type="submit" value = "Log in">
+					<input name="submit" type="submit" value = "Log in">
 				</p>
 				<p>
-				<a href="../../php/login/test.php">Signup</a>
+				<a href="php/login/test.php">Signup</a>
 				</p>
 
 			</ul>
@@ -65,9 +103,17 @@ $errors = array();
 
 </div>
 
+<?php
+include 'mysql.php';
+?>
+
 
 <div id="footer">
 Copyright © Grove.com
 </div>
+
+
+
+
 
 
