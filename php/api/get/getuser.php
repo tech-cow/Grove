72b7add00fd2eabd5abc2ed07f140b046a6f47dc
spencehood <spho6674@colorado.edu>
2014-12-07 16:53:11 -0700

    Added comments for Doxygen, running Doxygen next

--- php/api/get/getuser.php
+++ php/api/get/getuser.php
@@ -2,6 +2,15 @@
 require_once '../../classes/db/DB.class.php';
 //require_once '../../classes/controller/UserTools.class';
 
+/*! @mainpage GetUser()
+* @author Simon R-G
+* This function uses the sql connection to get info about the user from the database.
+*
+* @brief The point of this function is to return some info about the current user.  It will show up on the homepage and be necessary in matching him or her up with other users.
+* @return The values returned are the user id and the user's name.
+*
+*/
+
 function getuser(){ 
 	$db = new DB();
 	$db->connect();
