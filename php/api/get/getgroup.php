93444cd853edc55a2924dc3f5ca09aa93f978f72
spencehood <spho6674@colorado.edu>
2014-12-07 16:50:16 -0700

    Added comments for Doxygen, running Doxygen next

--- php/api/get/getgroup.php
+++ php/api/get/getgroup.php
@@ -2,6 +2,15 @@
 require_once '../../classes/db/DB.class.php';
 //require_once '/php/classes/controller/GroupTools.class.php';
 
+/*! @mainpage GetGroup()
+*@author Simon R-G
+*This function uses the sql connection to get info about a study group of users from the database.
+*
+*@brief The point of this function is to return the primary group info.  It will show up on the homepage and group page, and is a pivotal feature of Grove.
+*@return The value returned is the id of the group.
+*
+*/
+
 function getgroup(){ 
 	$db = new DB();
 	$db->connect();
